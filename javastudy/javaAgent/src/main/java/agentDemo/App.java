5163001e592fad11355ccfa007ede155b3e84be3
高威 <gw33973@ly.com>
2018-12-20 18:17:32 +0800

    添加javaagent测试代码

---
+++ javastudy/javaAgent/src/main/java/agentDemo/App.java
@@ -0,0 +1,11 @@
+package agentDemo;
+
+public class App {
+    public static void main(String[] args) {
+        hello();
+    }
+
+    public static void hello() {
+        System.out.println("this is agent-demo output");
+    }
+}
